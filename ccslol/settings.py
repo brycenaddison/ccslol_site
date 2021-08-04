c13a55f2e7ed8e05d841296f6d4c4e3de49fdfe1
Mino <28173801+shiminomi@users.noreply.github.com>
2021-08-04 14:53:37 -0400

    changed STATIC_ROOT

--- ccslol/settings.py
+++ ccslol/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 if 'HEROKU' in os.environ:
     import django_heroku
