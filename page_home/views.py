f26d7c0fe894d6d1ec69e4cd87061e110e67851c
Mino <28173801+shiminomi@users.noreply.github.com>
2021-09-10 12:57:47 -0400

    added riot.txt as requested by Riot Games, added a check to see if articles are available in blog page

--- page_home/views.py
+++ page_home/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def root(request):
     return render(request, 'page_home_html/main.html')
+
+
+def riot_txt(request):
+    return HttpResponse('df06b387-40e3-4a1a-a2db-1759c90669d9')
