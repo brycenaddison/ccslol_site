6b5f4dcc16ab919a0e31836d103d058052a61467
Mino <28173801+shiminomi@users.noreply.github.com>
2022-02-07 15:07:18 -0500

    added forward slash to path for riot's special requirements

--- page_home/urls.py
+++ page_home/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'page_home'
 urlpatterns = [
     path('', views.root, name='root'),
-    path('riot.txt', views.riot_txt, name='riot_txt')
+    path('/riot.txt', views.riot_txt, name='riot_txt')
 ]
