358f5b35025b118f282236110a78735a3cc714e2
Mino <28173801+shiminomi@users.noreply.github.com>
2022-02-16 18:41:52 -0500

    changed Game urls to redirect to spreadsheet for now

--- page_playoffs/views.py
+++ page_playoffs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def root(request):
+    return redirect('https://docs.google.com/spreadsheets/d/1cvBNafAxgwnJhwyTWmk3V7R8-lkgNElzbvi16W76wbA/edit#gid=1888404116')
     return render(request, 'page_playoffs_html/main.html')
--- page_schedule/views.py
+++ page_schedule/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def root(request):
+    return redirect('https://docs.google.com/spreadsheets/d/1cvBNafAxgwnJhwyTWmk3V7R8-lkgNElzbvi16W76wbA/edit#gid=1806252028')
     return render(request, 'page_schedule_html/main.html')
